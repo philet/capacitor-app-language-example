bdf5b94ee182c805c8bf820b5c3429212d133e44
Philippe Wüger <philippe.wueger@tbu.digital>
2023-07-19 12:07:04 +0200

    Initial commit

---
+++ www/js/capacitor-welcome.js
@@ -0,0 +1,115 @@
+window.customElements.define(
+  'capacitor-welcome',
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      Capacitor.Plugins.SplashScreen.hide();
+
+      const root = this.attachShadow({ mode: 'open' });
+
+      root.innerHTML = `
+    <style>
+      :host {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        display: block;
+        width: 100%;
+        height: 100%;
+      }
+      h1, h2, h3, h4, h5 {
+        text-transform: uppercase;
+      }
+      .button {
+        display: inline-block;
+        padding: 10px;
+        background-color: #73B5F6;
+        color: #fff;
+        font-size: 0.9em;
+        border: 0;
+        border-radius: 3px;
+        text-decoration: none;
+        cursor: pointer;
+      }
+      main {
+        padding: 15px;
+      }
+      main hr { height: 1px; background-color: #eee; border: 0; }
+      main h1 {
+        font-size: 1.4em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+      }
+      main h2 {
+        font-size: 1.1em;
+      }
+      main h3 {
+        font-size: 0.9em;
+      }
+      main p {
+        color: #333;
+      }
+      main pre {
+        white-space: pre-line;
+      }
+    </style>
+    <div>
+      <capacitor-welcome-titlebar>
+        <h1>Capacitor</h1>
+      </capacitor-welcome-titlebar>
+      <main>
+        <p>
+          <button class="button" id="get-language">Get language</button>
+        </p>
+        <p id="result"></p>
+      </main>
+    </div>
+    `;
+    }
+
+    connectedCallback() {
+      const self = this;
+
+      self.shadowRoot
+        .querySelector('#get-language')
+        .addEventListener('click', async function (e) {
+          const { Device } = Capacitor.Plugins;
+
+          const languageCode = await Device.getLanguageCode();
+          const appLanguageCode = await Device.getAppLanguageCode();
+
+          const deviceLanguageCodeElement = self.shadowRoot.querySelector('#result');
+          deviceLanguageCodeElement.innerHTML = 
+            'getLanguageCode: ' + languageCode.value;
+        });
+    }
+  },
+);
+
+window.customElements.define(
+  'capacitor-welcome-titlebar',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      const root = this.attachShadow({ mode: 'open' });
+      root.innerHTML = `
+    <style>
+      :host {
+        position: relative;
+        display: block;
+        padding: 15px 15px 15px 15px;
+        text-align: center;
+        background-color: #73B5F6;
+      }
+      ::slotted(h1) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-size: 0.9em;
+        font-weight: 600;
+        color: #fff;
+      }
+    </style>
+    <slot></slot>
+    `;
+    }
+  },
+);
